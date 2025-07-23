9e4f9d517a1d681e89ed573358ab820d8e570a57
Александров Никита <nikita.aleksandrov@monitel.ru>
2025-07-23 14:28:32 +0300

    add appsettings

--- MailSender/Program.cs
+++ MailSender/Program.cs
@@ -1,9 +1,13 @@
+using MailSender.Configs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection(nameof(SmtpSettings)));
 
 var app = builder.Build();
 
